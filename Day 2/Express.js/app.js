6f55a6f4a66e95339ffba452dae4a680bc609139
alizmq02 <alizmq02@gmail.com>
2024-02-07 19:21:42 +0300

    Day 3

--- Day 2/Express.js/app.js
+++ Day 2/Express.js/app.js
@@ -1,6 +1,4 @@
 // npm i --save-dev nodemon
 // npm i express
 
-// "script":{"devStart": "nodemon server.js"}
-
 // write the code here
---
+++ Day 3/app.js
@@ -0,0 +1,6 @@
+// Import nodemon dependency as dev dependency
+// npm i --save-dev nodemon
+// Import Express
+// npm i express
+// Make an instance of Express
+// Do get, and post functions
